f8d3d7d073b1a779ca69952631801db736c5fc54
Nathan <nathanmolson@gmail.com>
2025-09-23 13:27:55 -0700

    download dataset tar file in Job

--- common/cloud_storage.py
+++ common/cloud_storage.py
@@ -11,3 +11,10 @@ def upload(bucket_name: str, source_file_path: str, destination_blob_name: str):
         print(f"Uploaded {source_file_path} to gs://{bucket_name}/{destination_blob_name}")
     except PreconditionFailed:
         raise FileExistsError
+    
+    
+def download(bucket_name: str, remote_blob_name: str):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(remote_blob_name)
+    return blob.download_as_bytes()
--- mosaic/mosaic.py
+++ mosaic/mosaic.py
@@ -1,9 +1,21 @@
+import io
 import os
-import sys
+import tarfile
+import cloud_storage
+
 
 def main():
-    print(f"argv: {sys.argv}")
-    print(f"environ: {os.environ}")
+    bucket_name = os.environ["BUCKET"]
+    dataset_name = os.environ["DATASET"]
+    print(f"Downloading {dataset_name} from {bucket_name}")
+    dataset_archive = cloud_storage.download(bucket_name=bucket_name, remote_blob_name=dataset_name)
+    dataset_dir = "/dataset"
+    os.mkdir(dataset_dir)
+    with tarfile.open(fileobj=io.BytesIO(dataset_archive), mode="r") as tar:
+        tar.extractall(path=dataset_dir)
+
+    print("TODO: Process dataset")
+    print("SUCCESS!")
 
 
 if __name__ == "__main__":
