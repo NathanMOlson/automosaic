acec822a2ad903ea915746941adbe2bb2191251a
Nathan <nathanmolson@gmail.com>
2025-09-23 22:19:06 -0700

    working keepalive, with hardcoded paths and params

---
+++ batcher/keepalive.py
@@ -0,0 +1,52 @@
+import requests
+import time
+import urllib
+import google.auth.transport.requests
+import google.oauth2.id_token
+from concurrent.futures import ThreadPoolExecutor
+
+
+class KeepAlive:
+    def __init__(self, service_url: str, keepalive_time: float) -> None:
+        self.service_url = service_url
+        self.endpoint = f"{service_url}/keepalive"
+        self.keepalive_time = keepalive_time
+        self.executor = ThreadPoolExecutor()
+        self.future = self.executor.submit(self.task)
+        self.start_time = time.time()
+
+    def poke(self) -> None:
+        self.start_time = time.time()
+
+    def task(self) -> None:
+        while True:
+            time.sleep(1)
+            if time.time() - self.start_time < self.keepalive_time:
+                req = urllib.request.Request(self.endpoint)
+
+                auth_req = google.auth.transport.requests.Request()
+                id_token = google.oauth2.id_token.fetch_id_token(auth_req, self.service_url)
+
+                req.add_header("Authorization", f"Bearer {id_token}")
+                urllib.request.urlopen(req)
+
+
+def make_authorized_get_request(endpoint, audience):
+    """
+    make_authorized_get_request makes a GET request to the specified HTTP endpoint
+    by authenticating with the ID token obtained from the google-auth client library
+    using the specified audience value.
+    """
+
+    # Cloud Run uses your service's hostname as the `audience` value
+    # audience = 'https://my-cloud-run-service.run.app/'
+    # For Cloud Run, `endpoint` is the URL (hostname + path) receiving the request
+    # endpoint = 'https://my-cloud-run-service.run.app/my/awesome/url'
+
+    req = urllib.request.Request(endpoint)
+
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
+
+    req.add_header("Authorization", f"Bearer {id_token}")
+    response = urllib.request.urlopen(req)
--- batcher/upload_server.py
+++ batcher/upload_server.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
 from tempfile import mkstemp
 import os
+import time
 from batcher import Batcher
+from keepalive import KeepAlive
 
 app = Flask(__name__)
 batcher = Batcher()
+watchdog = KeepAlive("https://batcher-436396529778.us-west1.run.app", 30)
 
 @app.route("/")
 def hello():
     return "Lab 308 Upload Server"
+
+@app.route("/keepalive")
+def keepalive():
+    print("keepalive")
+    time.sleep(1)
+    return ""
 
 @app.route('/image', methods = ['POST'])
 def upload():
@@ -24,5 +33,6 @@ def upload():
     with os.fdopen(fd, 'wb') as f:
         file.save(f)
     batcher.on_new_file(filepath)
+    watchdog.poke()
 
     return jsonify({'message': f'File {file.filename} saved to {filepath}'}), 200
